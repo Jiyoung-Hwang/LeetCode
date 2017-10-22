9a39fb3519fa9175f014f8128ace23edbf9c0dc4
jyhwang <jyhwang@coupang.com>
2017-10-22 16:40:00 -0700

    [LeetCode] 169. Majority Element

--- Test.java
+++ Test.java
@@ -4,13 +4,32 @@ import java.util.HashMap;
 public class Test {
     public static void main(String args[]) throws Exception{
 
-        int[] nums = new int[]{3,2,1,1,1,5,1};
+        int[] nums = new int[]{1,3,5,7,3,3,3};
 
         System.out.println(Test.majorityElement_myApproach(nums));
         System.out.println(Test.majorityElement_myApproach2(nums));
         System.out.println(Test.majorityElement_best(nums));
+        System.out.println(Test.majorityElement_best2(nums));
     }
 
+    private static int majorityElement_best2(int[] nums){
+        int[] bitCnt = new int[32];
+
+        for(int num: nums){
+            for(int i=0;i<32;i++){
+                if((num>>i & 1) == 1) bitCnt[i]++;
+            }
+        }
+
+        int result = 0;
+        for(int i=0;i<32;i++){
+            if(bitCnt[i] > nums.length/2)
+                result = result | (1<<i);
+        }
+
+        return result;
+
+    }
     private static int majorityElement_best(int[] nums){
         int major = nums[0];
         int cnt = 0;
